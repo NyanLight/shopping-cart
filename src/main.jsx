d18dc2c831d7c5515511d4862602cf7ac28571f2
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-05-18 00:49:42 +0400

    Add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Goods } from './pages/Goods/Goods'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Goods } from "./pages/Goods/Goods";
+import { Cart } from "./pages/Cart/Cart";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Goods />,
+    errorElement: <div>404 not found</div>,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Goods />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
