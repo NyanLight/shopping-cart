b0392fc6aad0336951895366fcc25c260908d65f
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-06-09 18:10:33 +0400

    Add counter of cart's items quantity to Header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router";
-import styles from './Header.module.css'
-import heartIcon from '../../assets/heart.svg'
-import cartIcon from '../../assets/cart.svg'
-
+import styles from "./Header.module.css";
+import heartIcon from "../../assets/heart.svg";
+import cartIcon from "../../assets/cart.svg";
+import { useContext } from "react";
+import { ShopContext } from "../Wrapper/context";
 
 export function Header() {
+  const { cart } = useContext(ShopContext);
+  const count = cart.reduce((prev, curr) => prev + curr.quantity, 0);
+
   return (
     <header className={styles.header}>
       <h1 className={styles.companyName}>
@@ -12,15 +16,16 @@ export function Header() {
       </h1>
       <div className={styles.icons}>
         <div className={styles.icon}>
-            <Link to='/favorite'>
-            <img src={heartIcon}/>
-            </Link>
-            </div>
+          <Link to="/favorite">
+            <img src={heartIcon} />
+          </Link>
+        </div>
         <div className={styles.icon}>
-            <Link to="/cart">
+          <Link to="/cart">
             <img src={cartIcon} />
-            </Link>
-            </div>
+          </Link>
+          <div className={styles.counter}>{count}</div>
+        </div>
       </div>
     </header>
   );
